522514fb58d25933a1a12fedabf1f0bfd1e5145e
shamsWMM <shams.alsaegh@gmail.com>
2024-11-12 21:50:04 +1100

    Edit default CreatedOn value

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -10,6 +10,7 @@ namespace api.Models
         public int Id { get; set; }
         public string Title { get; set; } = String.Empty;
         public string Content { get; set; } = String.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
         public Stock? Stock { get; set; }
     }
